ae6df03a88fcf41cbf7e44b5d9f77cea38472a5f
mojahid-belaman <mojahidbelaman@gmail.com>
2022-03-13 23:35:47 +0100

    shuffle cards and flip just two cards and block all cards and add sounds

--- js/script.js
+++ js/script.js
@@ -12,5 +12,90 @@ myVar.addEventListener('click', function () {
     document.querySelector('.splash-screen').remove();
 })
 
+let blocksContainer = document.querySelector('.memory-container');
 
+let duration = 1000;
 
+let blocks = Array.from(blocksContainer.children);
+
+let orderRange = Array.from(Array(blocks.length).keys());
+
+shuffle(orderRange);
+
+blocks.forEach((element, index) => {
+    element.style.order = orderRange[index]; 
+    element.addEventListener('click', function() {
+        flipBlock(element);
+    });
+});
+
+function shuffle(array) {
+
+    let curr = array.length,
+        tmp,
+        random;
+    
+    while (curr > 0) {
+        
+        random = Math.floor(Math.random() * curr);
+
+        curr -= 1;
+
+        tmp = array[curr];
+        array[curr] = array[random];
+        array[random] = tmp;
+    }
+
+    return array;
+
+}
+
+function flipBlock(blockEle) {
+    
+    blockEle.classList.add('isflipped');
+
+    let flippedAll = blocks.filter(flipBlock => flipBlock.classList.contains('isflipped'));
+
+    if (flippedAll.length == 2)
+    {
+        stopClick();
+
+        isMatch(flippedAll[0], flippedAll[1]);
+    }
+}
+
+function stopClick() {
+    
+    blocksContainer.classList.add('blocking');
+    
+    setTimeout(() => {
+        blocksContainer.classList.remove('blocking');
+    }, duration);
+}
+
+function isMatch(firstBlock, secondBlock)
+{
+    let numTry = document.querySelector('.wrong span');
+
+    if (firstBlock.dataset.tech === secondBlock.dataset.tech) {
+        
+        secondBlock.classList.remove('isflipped');
+        firstBlock.classList.remove('isflipped');
+        
+        firstBlock.classList.add('has-match');
+        secondBlock.classList.add('has-match');
+
+        document.getElementById('success').play();
+    }
+    else {
+        numTry.innerHTML = parseInt(numTry.textContent) + 1;
+
+        setTimeout(() => {
+            
+            secondBlock.classList.remove('isflipped');
+            firstBlock.classList.remove('isflipped');
+        }, duration);
+
+        document.getElementById('failed').play();
+    }
+}
